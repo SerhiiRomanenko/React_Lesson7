2a397035f1d6b3839709085b32bee6d35716eeb4
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2021-12-19 15:36:28 +0200

    third commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { ShipsList } from "./ShipsList/ShipsList";
-import { ShipsSearch } from "./ShipsList/ShipsSearch";
 import Loader from "react-loader-spinner";
 
 export class Main extends Component {
@@ -11,40 +10,51 @@ export class Main extends Component {
       error: null,
       data: null,
     };
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
 
-    fetch("https://swapi.dev/api/starships/?search=" + e.currentTarget[0].value)
-      .then((res) => {
-        this.setState({
-          status: "loading",
-          error: null,
-          data: null,
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+
+      fetch(
+        "https://swapi.dev/api/starships/?search=" + e.currentTarget[0].value
+      )
+        .then((res) => {
+          this.setState({
+            status: "loading",
+            error: null,
+            data: null,
+          });
+          return res.json();
+        })
+        .then((dataJSON) => {
+          this.setState({
+            status: "success",
+            error: null,
+            data: dataJSON,
+          });
+        })
+        .catch((err) => {
+          this.setState({
+            status: "error",
+            error: err.message,
+            data: null,
+          });
         });
-        return res.json();
-      })
-      .then((dataJSON) => {
-        this.setState({
-          status: "success",
-          error: null,
-          data: dataJSON,
-        });
-      })
-      .catch((err) => {
-        this.setState({
-          status: "error",
-          error: err.message,
-          data: null,
-        });
-      });
-    e.currentTarget[0].value = "";
-  };
+      e.currentTarget[0].value = "";
+    };
+  }
+
   render() {
     const { status, error, data } = this.state;
+
     return (
       <div style={{ textAlign: "center" }}>
-        <ShipsSearch handleSubmit={this.handleSubmit} />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <span>Enter search text: </span>
+            <input />
+          </label>
+          <button type="submit">Submit / Clear</button>
+        </form>
         {status === "initial" || status === "loading" ? (
           <div style={{ color: "blue", textAlign: "center" }}>
             <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
